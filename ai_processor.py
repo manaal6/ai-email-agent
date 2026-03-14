ef0901a5375166ebbb6d8ef4abbea3252b73a10d
manaal6 <manaalpervaiz6@gmail.com>
2026-03-14 22:20:09 +0500

    fix gemini model

--- ai_processor.py
+++ ai_processor.py
@@ -1,20 +1,26 @@
-from google import genai
 import os
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
 
 def generate_reply(email_text):
+
     prompt = f"""
-    A customer sent this email.
-    Email:
-    {email_text}
-    Write a professional reply for a business.
-    """
+A customer sent this email:
+
+{email_text}
+
+Write a short professional business reply.
+"""
+
     response = client.models.generate_content(
-        model="gemini-1.5-flash",
+        model="gemini-2.0-flash",
         contents=prompt
     )
+
     return response.text
